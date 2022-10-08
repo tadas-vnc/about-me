4a641ee643e75edaf372ef1b7dcea6b4d105c46d
tadas-vnc <115348455+tadas-vnc@users.noreply.github.com>
2022-10-08 13:19:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+$("#threelinesbox").click(function(){
+    if ($("#threelines").css("rotate") == "180deg"){
+        $("#threelines").css("rotate", "0deg");
+        $("#menu").css("height", "0px");
+        $("#menu").css("border-style", "");
+    }else{
+        $("#threelines").css("rotate", "180deg");
+        $("#menu").css("height", "200px");
+        $("#menu").css("border-style", "solid");
+    }
+})
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
